18dd23ca6772de50ab87f88afa4a05700d89c52d
Tyll Weiß <tyll.weiss.extern@axelspringer.com>
2017-09-05 11:46:48 +0200

    TASK: Fix the assertion of the plugins

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,13 @@
 const config = require('./index.js');
 
 describe('babel-preset-immowelt', () => {
-	it('should export a valid object structure that contains the flow-strip-types transform.', () => {
+	it('should export a valid object structure that contains the react-jsx and flow-strip-types transform.', () => {
 		expect(typeof config).toBe('object');
 		expect(config.plugins).toBeDefined();
 		expect(config.plugins instanceof Array).toBe(true);
-		expect(config.plugins[0]).toBe('transform-flow-strip-types');
+		expect(config.plugins).toEqual([
+			'transform-react-jsx',
+			'transform-flow-strip-types'
+		]);
 	});
 });
